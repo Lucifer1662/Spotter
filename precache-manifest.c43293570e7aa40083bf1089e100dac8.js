17deadc521a4e741e60846c6c64b802a761f5850
l.hawk99@hotmail.com <l.hawk99@hotmail.com>
2020-11-12 13:31:47 +1100

    Updates

---
+++ precache-manifest.c43293570e7aa40083bf1089e100dac8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3b1bd114a2c027c77cfc19577371bbb",
+    "url": "/Spotter/index.html"
+  },
+  {
+    "revision": "f9063a1e4d5ca58e1932",
+    "url": "/Spotter/static/css/main.9850759f.chunk.css"
+  },
+  {
+    "revision": "fe4e6d0c942f42a46437",
+    "url": "/Spotter/static/js/2.66fe66a1.chunk.js"
+  },
+  {
+    "revision": "9f523a7df1773e062a4d3213a45af159",
+    "url": "/Spotter/static/js/2.66fe66a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9063a1e4d5ca58e1932",
+    "url": "/Spotter/static/js/main.3f91b44a.chunk.js"
+  },
+  {
+    "revision": "fec62f66a54ae1ca8c83",
+    "url": "/Spotter/static/js/runtime-main.80962c0c.js"
+  }
+]);
