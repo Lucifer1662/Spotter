f36505676389656977b993315d070365c3031030
l.hawk99@hotmail.com <l.hawk99@hotmail.com>
2020-11-12 16:43:05 +1100

    added option to not show bones

--- src/WebCamPose.tsx
+++ src/WebCamPose.tsx
@@ -5,6 +5,8 @@ import VideoWithArmature from './VideoWithArmature';
 import WebcamCapture from './WebCameCapture';
 import estimatePoseOnImage from './estimatePoseOnImage'
 import { setPose as setPoseGlobal } from './pose';
+import {IconButton, FormControlLabel, Checkbox} from "@material-ui/core"
+import {Menu } from '@material-ui/core/'
 
 interface Props {
   parents?: any[]
@@ -14,6 +16,7 @@ export default function WebCamePose({ parents = [] }: Props) {
 
   let [{ video }] = useUniversalState("WebCamCapture", parents);
   let [pose, setPose] = useState<Pose | undefined>();
+  let [showBones, setShowBones] = useState(true);
 
   if (pose) {
     setPoseGlobal(pose);
@@ -38,7 +41,14 @@ export default function WebCamePose({ parents = [] }: Props) {
   }, [video])
 
   return <div style={{ position: 'relative' }}>
+    
     <WebcamCapture parents={parents} />
+    {showBones && 
     <VideoWithArmature pose={pose} width={video ?.width} height={video ?.height} style={{ position: 'absolute', left: 0, right: 0, width: video ?.width, height: video ?.height }} />
+    }
+    <FormControlLabel
+            control={<Checkbox name="showBones" value={!showBones} onChange={(e) => setShowBones(e.target.checked)} />}
+            label="Show Bones"
+        />
   </div>
 }
